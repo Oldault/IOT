324c67545727cf2b01d3cf9a8a58b6261a354630
Simon <simon@42.com>
2025-09-20 15:25:46 +0000

    p1

--- p1/scripts/server_setup.sh
+++ p1/scripts/server_setup.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
-apk add --no-cache curl openssh
-
-ssh-keygen -A
-rc-update add sshd
-
-curl -sfL https://get.k3s.io | sh -
-rc-update add k3s default
-rc-service k3s start
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server" sh -s - --token $1 --node-ip $2
 
 for i in $(seq 1 60); do
     if [ -f /var/lib/rancher/k3s/server/node-token ]; then
@@ -21,7 +14,7 @@ if [ ! -f /var/lib/rancher/k3s/server/node-token ]; then
     exit 1
 fi
 
-chmod a+r /var/lib/rancher/k3s/server/node-token
+sudo chmod a+r /etc/rancher/k3s/k3s.yaml
 cp /var/lib/rancher/k3s/server/node-token /vagrant/config/k3s_token.txt
 
 if ! curl -k -s https://127.0.0.1:6443/version >/dev/null; then
@@ -29,3 +22,5 @@ if ! curl -k -s https://127.0.0.1:6443/version >/dev/null; then
 fi
 
 echo "k3s server installed and running"
+
+alias k=kubectl
--- p1/scripts/worker_setup.sh
+++ p1/scripts/worker_setup.sh
@@ -1,27 +1,3 @@
 #!/bin/sh
 
-apk add --no-cache curl openssh
-
-ssh-keygen -A
-rc-update add sshd
-
-while [ ! -f /vagrant/config/k3s_token.txt ]; do
-  sleep 1
-done
-
-K3S_TOKEN=$(cat /vagrant/config/k3s_token.txt)
-
-curl -sfL https://get.k3s.io | sh -
-
-export K3S_URL=https://192.168.56.110:6443
-export K3S_TOKEN=$K3S_TOKEN
-
-rc-update add k3s default   
-rc-service k3s start
-
-if rc-service k3s status | grep -q 'started'; then
-  echo "k3s agent is up and running"
-else
-  echo "WARNING: k3s agent failed to start – check $INSTALL_LOG"
-fi
-
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$2:6443 --token $1 --node-ip $3" sh -s -
