9a94cfc5cb5e1ae83b5035b6c0359b173913f0e6
Nakusu <nakusu35@hotmail.fr>
2025-10-11 12:28:08 +0200

    [p3] create cluster at start

--- p3/install.sh
+++ p3/install.sh
@@ -5,3 +5,7 @@ curl -s https://get.docker.com | sh
 sudo usermod -aG docker $USER
 
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+
+k3d cluster create $USER
+
+kubectl config use-context k3d-$USER
