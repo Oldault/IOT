8937738b34276e9a6b784b92ab3951f6b9d42c6b
Simon <simon@42.com>
2025-10-11 10:54:40 +0000

    [p3] cleaning

--- p3/src/scripts/install.sh
+++ p3/src/scripts/install.sh
@@ -4,6 +4,7 @@ if ! command -v docker &> /dev/null; then
     echo "Docker not found. Installing Docker..."
     curl -s https://get.docker.com | sh
     sudo usermod -aG docker $USER
+    sudo usermod -aG docker svolodin
 fi
 
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
